ce8a0cbb040053332677d3573e3704a5d931cf22
dibalass <123352922+dibalass@users.noreply.github.com>
2023-06-20 13:09:38 +0500

    Add files via upload

---
+++ App1/App1/Calc.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.CommunityToolkit.Extensions;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Calc :ContentPage
+    {
+        public Calc ()
+        {
+            InitializeComponent();
+        }
+
+        private void SliderValueChange (object sender, ValueChangedEventArgs e)
+        {
+            SliderLabel.Text = $"{Slider.Value}%";
+            try
+            {
+                if (LoanEntry.Text != "" && MonthEntry.Text != "")
+                {
+                    Calculation(LoanEntry.Text, MonthEntry.Text, PaymentTypePicker.SelectedIndex, Slider.Value);
+                } else
+                {
+                    MonthlyPaymentLabel.Text = "Ежемесячный платеж: ...";
+                    TotalLabel.Text = "Общая сумма: ...";
+                    OverpaymentLabel.Text = "Переплата: ...";
+                }
+            } catch (Exception)
+            {
+                MonthlyPaymentLabel.Text = "Ежемесячный платеж:...";
+                TotalLabel.Text = "Общая сумма:...";
+                OverpaymentLabel.Text = "Переплата:...";
+                this.DisplayToastAsync("Введи число");
+            }
+        }
+
+        private void Calculation (string Summa, string Month, int TypePayment, double Slider)
+        {
+            if (Convert.ToDouble(Month) != 0 && Convert.ToDouble(Summa) != 0)
+            {
+                switch (TypePayment)
+                {
+                    case 0:
+                    {
+                        double EveryMonthPay = (Convert.ToDouble(Summa) + Convert.ToDouble(Summa) * Slider / 100) / Convert.ToDouble(Month);
+
+                        MonthlyPaymentLabel.Text = $"Ежемесячный платеж: {Math.Round(((Convert.ToDouble(Summa) + Convert.ToDouble(Summa) * Slider / 100) / Convert.ToDouble(Month)), 2)}";
+                        TotalLabel.Text = $"Общая сумма: {Math.Round(EveryMonthPay * Convert.ToDouble(Month), 2)}";
+                        OverpaymentLabel.Text = $"Переплата: {Math.Round(EveryMonthPay * int.Parse(Month) - double.Parse(Summa), 2)}";
+                    }
+                    break;
+                    default:
+                    {
+                        MonthlyPaymentLabel.Text = "Ежемесячный платеж:...";
+                        TotalLabel.Text = "Общая сумма:...";
+                        OverpaymentLabel.Text = "Переплата:...";
+                    }
+                    break;
+                }
+            } else
+            {
+                MonthlyPaymentLabel.Text = "Ежемесячный платеж:...";
+                TotalLabel.Text = "Общая сумма:...";
+                OverpaymentLabel.Text = "Переплата:...";
+            }
+        }
+    }
+}
---
+++ App1/App1/CurrencyExchanger.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.CommunityToolkit.Extensions;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CurrencyExchanger : ContentPage
+    {
+        public CurrencyExchanger()
+        {
+            InitializeComponent();
+        }
+
+        private void USDChanged (object sender, TextChangedEventArgs e)
+        {
+            if (usdEntry.Text != "")
+            {
+                try
+                {
+                    eurLabel.Text = (double.Parse(usdEntry.Text) * 1.075).ToString();
+                } catch (Exception)
+                {
+                    this.DisplayToastAsync("Введи число");
+                    usdEntry.Text = "";
+                }
+            }
+        }
+    }
+}
